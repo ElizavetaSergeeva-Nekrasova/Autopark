3cb68c71a8d046303f10e7d31cf1e50d3bb731da
Elizaveta Sergeeva-Nekrasova <elizaveta.sergeevanekrasova@gmail.com>
2022-07-05 17:08:17 +0300

    done Level3

--- src/TechnicalSpecialist.java
+++ src/TechnicalSpecialist.java
@@ -1,2 +1,65 @@
 public class TechnicalSpecialist {
+    public static final int LOWER_LIMIT_MANUFACTURE_YEAR = 1886;
+
+    static public boolean validateManufactureYear(int year) {
+        if (year >= LOWER_LIMIT_MANUFACTURE_YEAR
+                && year < 9999) {
+            return true;
+        }
+
+        return false;
+    }
+
+    static public boolean validateMileage(int mileage) {
+        if (mileage >= 0) {
+            return true;
+        }
+
+        return false;
+    }
+
+    static public boolean validateWeight(double weight) {
+        if (weight >= 0) {
+            return true;
+        }
+
+        return false;
+    }
+
+    static public boolean validateColor(Color color) {
+        if (color != null) {
+            return true;
+        }
+
+        return false;
+    }
+
+    static public boolean validateVehicleType(VehicleType vehicleType) {
+        if (vehicleType.getTaxCoefficient() >= 0.0d
+                && vehicleType.getTypeName() != ""
+                && vehicleType.getTypeName() != null) {
+            return true;
+        }
+
+        return false;
+    }
+
+    static public boolean validateRegistrationNumber(String number) {
+        if (number != null) {
+            String regex = "\\d{4}\\s{1}[A-Z]{2}(-)\\d{1}";
+            if (number.matches(regex)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    static public boolean validateModelName(String name) {
+        if (name != "") {
+            return true;
+        }
+
+        return false;
+    }
 }

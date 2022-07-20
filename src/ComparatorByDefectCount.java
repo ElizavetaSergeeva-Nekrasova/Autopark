a056384f1d54364fa979227624d2bbf3d3db4282
Elizaveta Sergeeva-Nekrasova <elizaveta.sergeevanekrasova@gmail.com>
2022-07-20 15:51:24 +0300

    clean code

--- src/ComparatorByDefectCount.java
+++ src/ComparatorByDefectCount.java
@@ -3,6 +3,6 @@ import java.util.Comparator;
 public class ComparatorByDefectCount implements Comparator<Vehicle> {
     @Override
     public int compare(Vehicle o1, Vehicle o2) {
-        return (MechanicService.getSumNumberOfBreaks(o2)) - (MechanicService.getSumNumberOfBreaks(o1));
+        return Integer.compare(MechanicService.getSumNumberOfBreaks(o2), MechanicService.getSumNumberOfBreaks(o1));
     }
 }

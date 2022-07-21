214d432ae21d7351262effcd9dab50496e0b199e
Elizaveta Sergeeva-Nekrasova <elizaveta.sergeevanekrasova@gmail.com>
2022-07-21 23:37:16 +0300

    add packages

--- src/ComparatorByDefectCount.java
+++
@@ -1,8 +0,0 @@
-import java.util.Comparator;
-
-public class ComparatorByDefectCount implements Comparator<Vehicle> {
-    @Override
-    public int compare(Vehicle o1, Vehicle o2) {
-        return Integer.compare(MechanicService.getSumNumberOfBreaks(o2), MechanicService.getSumNumberOfBreaks(o1));
-    }
-}
--- src/Randomizer.java
+++
@@ -1,9 +0,0 @@
-public class Randomizer {
-    public static int getRandomFromZeroToNMinusOne(int n) {
-        return (int) (Math.random() * n);
-    }
-
-    public static int getRandomFromOneToN(int n) {
-        return (int) ((Math.random() * n) + 1);
-    }
-}
